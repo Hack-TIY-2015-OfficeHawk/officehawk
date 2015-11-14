2ca10ab2be4a2c6e293c9f5c4589d0dee0c7a5c1
violetaria <terri.chu@outlook.com>
2015-11-14 16:25:34 -0500

    model setup for beacon

--- app/models/beacon.rb
+++ app/models/beacon.rb
@@ -1,4 +1,11 @@
 class Beacon < ActiveRecord::Base
   has_many :alerts
   has_many :employees, through: :alerts
+
+  belongs_to :organization
+
+  # validates_uniqueness_of [:major, :minor], scope: :organization
+  # validates_uniqueness_of :name, scope: :organization_id
+
+  validates_presence_of :organization_id, :manjor, :minor, :uuid
 end
--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,5 +1,6 @@
 class Organization < ActiveRecord::Base
   has_many :employees
+  has_many :beacons
 
   validates_uniqueness_of :name
 end
