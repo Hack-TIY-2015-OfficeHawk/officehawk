38e0c3f21508a26b1d9355f1535c757b7370affe
violetaria <terri.chu@outlook.com>
2015-11-14 17:47:24 -0500

    org model tests complete

--- test/models/organization_test.rb
+++ test/models/organization_test.rb
@@ -4,4 +4,43 @@ class OrganizationTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "can create valid organization" do
+    organization = Organization.new(name: "ORG",
+                                    owner: "Terri")
+
+    assert organization.save
+
+    assert organization.errors.blank?
+  end
+
+  test "cannot create org with blank name" do
+    organization = Organization.new(owner: "Terri")
+
+    refute organization.save
+
+    assert organization.errors.present?
+  end
+
+  test "cannot create org with blank owner" do
+    organization = Organization.new(name: "ORG")
+
+    refute organization.save
+
+    assert organization.errors.present?
+  end
+
+  test "cannot create two orgs with same name" do
+    organization1 = Organization.new(name: "ORG",
+                                    owner: "Terri")
+
+    organization2 = Organization.new(name: "ORG",
+                                     owner: "Terri")
+
+    assert organization1.save
+    refute organization2.save
+
+    assert organization1.errors.blank?
+    assert organization2.errors.present?
+  end
 end
