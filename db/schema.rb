0d59fd949f4e16e7eb4d94ea01407e764046f47f
violetaria <terri.chu@outlook.com>
2015-11-14 16:21:46 -0500

    updated columns for beacons, adding major minor and organization_id columns, removing name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151114192449) do
+ActiveRecord::Schema.define(version: 20151114211453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,10 +26,12 @@ ActiveRecord::Schema.define(version: 20151114192449) do
   end
 
   create_table "beacons", force: :cascade do |t|
-    t.string   "name"
     t.string   "uuid"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "major",           null: false
+    t.string   "minor",           null: false
+    t.integer  "organization_id", null: false
   end
 
   create_table "employees", force: :cascade do |t|
