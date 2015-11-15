a1fb6c1d756ad89c127c6c26297398fee689fcfc
violetaria <terri.chu@outlook.com>
2015-11-14 19:44:07 -0500

    updated employee fixtures, added beacon model tests

--- test/models/beacon_test.rb
+++ test/models/beacon_test.rb
@@ -4,4 +4,44 @@ class BeaconTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "can create valid beacon" do
+    beacon = organizations(:one).beacons.new(uuid: "testuuid",
+                                             major: "100",
+                                             minor: "1001")
+
+    assert beacon.save
+
+    assert beacon.errors.blank?
+  end
+
+  test "cannot create beacon with blank uuid" do
+    beacon = organizations(:one).beacons.new(major: "100",
+                                             minor: "1001")
+
+    refute beacon.save
+
+    assert beacon.errors.present?
+  end
+
+  test "cannot create beacon with blank major" do
+    beacon = organizations(:one).beacons.new(uuid: "testuuid",
+                                             minor: "1001")
+
+    refute beacon.save
+
+    assert beacon.errors.present?
+  end
+
+
+  test "cannot create beacon with blank minor" do
+    beacon = organizations(:one).beacons.new(uuid: "testuuid",
+                                             major: "1001")
+
+    refute beacon.save
+
+    assert beacon.errors.present?
+  end
+
+
 end
